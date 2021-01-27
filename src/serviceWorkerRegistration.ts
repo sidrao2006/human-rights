92fd3b081d88edb2a481e817f46e9d9980aa9ea2
Aneesh Rao <sidrao2006@gmail.com>
2021-01-27 10:24:45 +0530

    Service worker code maintenance update

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -1,4 +1,4 @@
-// This optional code is used to register a service worker.
+// This code is used to register a service worker.
 // register() is not called by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -7,15 +7,12 @@
 // existing tabs open on the page have been closed, since previously cached
 // resources are updated in the background.
 
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://cra.link/PWA
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.0/8 are considered localhost for IPv4.
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 type Config = {
@@ -30,7 +27,7 @@ export function register(config?: Config) {
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      // serve assets
       return;
     }
 
@@ -44,9 +41,8 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://cra.link/PWA'
+          console.info('Service Worker: ' +
+            'This web app is being served cache-first by a service worker'
           );
         });
       } else {
@@ -72,9 +68,9 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              console.info('Service Worker: ' +
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
+                'tabs for this page are closed.'
               );
 
               // Execute callback
@@ -85,7 +81,9 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              console.info('Service Worker: ' +
+                'Content is cached for offline use.'
+              );
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -97,7 +95,10 @@ function registerValidSW(swUrl: string, config?: Config) {
       };
     })
     .catch((error) => {
-      console.error('Error during service worker registration:', error);
+      console.error('Service Worker: ' +
+        'Error during service worker registration:',
+        error
+      );
     });
 }
 
@@ -125,7 +126,9 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
       }
     })
     .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.warn('Service Worker: ' +
+        'No internet connection found. App is running in offline mode.'
+      );
     });
 }
 
